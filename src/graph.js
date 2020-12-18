e1a40441be903ddb686c426d5ae2f4234e7a2248
raffmares <crmares@tuta.io>
2020-12-18 19:44:41 +0000

    Major update: using line series and time markers

--- src/graph.js
+++ src/graph.js
@@ -5,25 +5,48 @@ import {
   XAxis,
   YAxis,
   HorizontalGridLines,
-  MarkSeriesCanvas
+  LineSeriesCanvas
 } from 'react-vis';
 import moment from 'moment';
 
-export default function Graph( {graphVisible, graphData, maxTimeVal, graphMaxY, minTS, tsInterval} ) {
+export default function Graph( {graphData, maxTimeVal, graphMaxY, minTS, tsInterval, trackTime0, trackTime} ) {
   return (
-    <div className='graph' style={{visibility: graphVisible}}>
-      <FlexibleWidthXYPlot
-        height={140}
-        xDomain={[0, maxTimeVal]}
-        yDomain={[0, graphMaxY]}
-      >
-        <HorizontalGridLines />
-        <XAxis tickFormat={d => moment.utc((d * tsInterval + minTS) * 1000).format('YYYY-MM-DD')} />
-        <YAxis tickFormat={d => Math.round(d / 10) / 100} />
-        {graphData.map(props => (
-          <MarkSeriesCanvas {...props} />
-        ))}
-      </FlexibleWidthXYPlot>
-    </div>
+    <FlexibleWidthXYPlot
+      height={140}
+      margin={{left: 50, right: 25}}
+      xDomain={[0, maxTimeVal]}
+      yDomain={[0, graphMaxY]}
+    >
+      <HorizontalGridLines />
+      <XAxis
+        tickFormat={d => moment.utc((d * tsInterval + minTS) * 1000).format('YYYY-MM-DD')}
+        tickTotal={12}
+        tickSizeInner={0}
+      />
+      <YAxis
+        tickFormat={d => Math.round(d / 1000)}
+        tickTotal={4}
+        tickSizeInner={0}
+      />
+      {graphData.map(props => (
+        <LineSeriesCanvas
+          {...props}
+          opacity={0.6}
+          strokeWidth={1.6}
+        />
+      ))}
+      <LineSeriesCanvas
+        data={[{x:trackTime0, y:0}, {x:trackTime0, y:graphMaxY}]}
+        color={'#ffffff'}
+        opacity={0.4}
+        strokeWidth={3}
+      />
+      <LineSeriesCanvas
+        data={[{x:trackTime, y:0}, {x:trackTime, y:graphMaxY}]}
+        color={'#ffffff'}
+        opacity={0.4}
+        strokeWidth={3}
+      />
+    </FlexibleWidthXYPlot>
   );
 }
